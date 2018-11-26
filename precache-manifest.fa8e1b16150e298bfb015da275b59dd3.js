c47d7c2bc807db579c7a19da4f576fed525dac1b
joesyoon <joesyoon@uw.edu>
2018-11-25 21:35:25 -0800

    Updates

---
+++ precache-manifest.fa8e1b16150e298bfb015da275b59dd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d3f79497392eebb1d5",
+    "url": "/Food-for-Thought/static/js/runtime~main.c6d3f794.js"
+  },
+  {
+    "revision": "cd359b5b456d9c0c98a3",
+    "url": "/Food-for-Thought/static/js/main.cd359b5b.chunk.js"
+  },
+  {
+    "revision": "7b2b142d634fe76a86bd",
+    "url": "/Food-for-Thought/static/js/1.7b2b142d.chunk.js"
+  },
+  {
+    "revision": "cd359b5b456d9c0c98a3",
+    "url": "/Food-for-Thought/static/css/main.c41ed005.chunk.css"
+  },
+  {
+    "revision": "3c33e11897636159a139dfa52b285525",
+    "url": "/Food-for-Thought/index.html"
+  }
+];
